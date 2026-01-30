e188daa80fdb15d92af0f5f326b4ccf0d1162f87
watanabeeito <zm24160@josai.ac.jp>
2026-01-30 23:39:32 +0900

    Update ex8-8.js

--- ex8-8.js
+++ ex8-8.js
@@ -1,2 +1,2 @@
-function imagechange1(){document.getElementById('logo2').src='./image/キスマイ.jpg';}
+function imagechange1(){document.getElementById('logo2').src='./image/kis-my2.jpg';}
 function imagechange2(){document.getElementById('logo2').src='./image/kis-my.jpg';}
